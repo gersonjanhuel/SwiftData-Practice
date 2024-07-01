455d104281cd3243e12a4a4f2a3f05aaa65bc72e
Gerson Janhuel <gersonjanhuel@gmail.com>
2024-07-01 13:44:17 +0700

    Initial Commit

---
+++ SwiftData-Practice/SwiftData_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_PracticeApp.swift
+//  SwiftData-Practice
+//
+//  Created by Gerson Janhuel on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
